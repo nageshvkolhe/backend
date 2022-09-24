cab8ad8af2a8965956cb058e2a69e8464119ffef
nageshvilaskolhe <nagesh.kolhe@goniyo.com>
2022-09-24 17:59:12 +0530

    Added APIs for Get & Post Users

--- models/user.js
+++ models/user.js
@@ -1,12 +1,55 @@
 const mongoose = require("mongoose");
 
 const userSchema = mongoose.Schema({
-  name: String,
-  image: String,
-  countInStock: {
-    type: Number,
+  name: {
+    type: String,
     required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  passwordHash: {
+    type: String,
+    required: true,
+  },
+  street: {
+    type: String,
+    default: "",
+  },
+  apartment: {
+    type: String,
+    default: "",
+  },
+  city: {
+    type: String,
+    default: "",
+  },
+  zip: {
+    type: String,
+    default: "",
+  },
+  country: {
+    type: String,
+    default: "",
+  },
+  phone: {
+    type: String,
+    required: true,
+  },
+  isAdmin: {
+    type: Boolean,
+    required: false,
   },
 });
 
-exports.user = mongoose.model('User', userSchema);
+userSchema.virtual("id").get(function () {
+  return this._id.toHexString();
+});
+
+userSchema.set("toJSON", {
+  virtuals: true,
+});
+
+exports.User = mongoose.model("User", userSchema);
+exports.userSchema = userSchema;
--- routes/users.js
+++ routes/users.js
@@ -1,15 +1,47 @@
-const {User} = require('../models/user')
+const { User } = require("../models/user");
 const express = require("express");
 const router = express.Router();
+const bcrypt = require("bcryptjs");
 
+router.get(`/`, async (req, resp) => {
+  const userList = await User.find().select("-passwordHash");
 
-router.get(`/`,async (req, resp) => {
-    const userList = await User.find();
-   
-    if(!userList){
-        resp.status(500).json({success:false})
-    }
-    resp.send(userList);
+  if (!userList) {
+    resp.status(500).json({ success: false });
+  }
+  resp.send(userList);
+});
+
+router.get(`/:id`, async (req, resp) => {
+  const user = await User.findById(req.params.id).select("-passwordHash");
+
+  if (!user) {
+    resp
+      .status(500)
+      .json({ message: "The Ueser with given Id was not found !!" });
+  }
+  resp.send(user);
+});
+
+router.post(`/`, async (req, resp) => {
+  let user = new User({
+    name: req.body.name,
+    email: req.body.email,
+    passwordHash: bcrypt.hashSync(req.body.passwordHash, 10), //secret
+    street: req.body.street,
+    apartment: req.body.apartment,
+    city: req.body.city,
+    zip: req.body.zip,
+    country: req.body.country,
+    phone: req.body.phone,
+    isAdmin: req.body.isAdmin,
   });
-  
+  user = await user.save();
+
+  if (!user) {
+    return resp.status(500).send("The User cannot be create !!");
+  }
+  resp.send(user);
+});
+
 module.exports = router;
